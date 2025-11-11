d2f164b1a7fda8e580a1c59d17f5549e715c9dc9
Doanh <viaicamon28@gmail.com>
2025-11-11 16:09:53 +0700

    update main

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import os
+import uvicorn
+
 from fastapi import FastAPI
 
 from app.api.v1.routers.auth import auth
 app = FastAPI()
 app.include_router(auth.router, tags = ["Auth"])
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port = port)
