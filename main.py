eaaab517aa2c5f32fb62fdfa18d8fed05690e9a6
Doanh <viaicamon28@gmail.com>
2025-11-11 15:40:29 +0700

    procfile

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from fastapi import FastAPI
 
 from app.api.v1.routers.auth import auth
-from app.api.v1.routers.home import home
-from app.api.v1.routers.admin import admin
-
-# email = "viaicamon28@gmail.com"
-# password = "utc@123"
-
 app = FastAPI()
 app.include_router(auth.router, tags = ["Auth"])
-# apptest.include_router(home.router, tags=["Home"])
-# apptest.include_router(admin.router, tags=["Admin"])
